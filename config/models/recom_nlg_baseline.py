520cfb609cc3878f901f960fadd129af802ceda0
wanghm92 <hongmin_wang@ucsb.edu>
2018-07-24 10:22:11 -0700

    some configurations

--- config/models/recom_nlg_baseline.py
+++ config/models/recom_nlg_baseline.py
@@ -12,24 +12,26 @@ def model():
       source_inputter=onmt.inputters.ParallelInputter([
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="source_words_vocabulary",
-              embedding_size=512),
+              embedding_size=None,
+              embedding_file_key="words_embedding"),
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="feature_vocabulary",
               embedding_size=64)],
           reducer=onmt.layers.ConcatReducer()),
       target_inputter=onmt.inputters.WordEmbedder(
           vocabulary_file_key="target_words_vocabulary",
-          embedding_size=512),
+          embedding_size=None,
+          embedding_file_key="words_embedding"),
       encoder=onmt.encoders.BidirectionalRNNEncoder(
-          num_layers=4,
-          num_units=512,
+          num_layers=1,
+          num_units=128,
           reducer=onmt.layers.ConcatReducer(),
           cell_class=tf.contrib.rnn.LSTMCell,
           dropout=0.3,
           residual_connections=False),
       decoder=onmt.decoders.AttentionalRNNDecoder(
-          num_layers=4,
-          num_units=512,
+          num_layers=1,
+          num_units=128,
           bridge=onmt.layers.CopyBridge(),
           attention_mechanism_class=tf.contrib.seq2seq.LuongAttention,
           cell_class=tf.contrib.rnn.LSTMCell,
