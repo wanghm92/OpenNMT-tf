label sharding bug fixed, loss normalizing bug fixed, TODO:[different weights for master/sub losses, TODOs in project, Eval file processing, add max_time param]

opennmt/decoders/hierarchical_rnn_decoder.py
@@ -210,17 +210,17 @@ class HierarchicalAttentionalRNNDecoder(AttentionalRNNDecoder):
     tf.logging.info(" >> [hierarchical_rnn_decoder.py decode] state = {}".format(state))
     tf.logging.info(" >> [hierarchical_rnn_decoder.py decode] length = {}".format(length))
     tf.logging.info(" >> [hierarchical_rnn_decoder.py decode] final_time = {}".format(final_time))
-    # add_dict_to_collection("debug", {"final_time": final_time,
-    #                                  "sequence_mask_sub_shape": tf.shape(sequence_mask_sub),
-                                     # "outputs.rnn_output shape": tf.shape(outputs.rnn_output),
-                                     # "outputs_sub.rnn_output shape": tf.shape(outputs_sub.rnn_output),
-                                     # "cell_state_c": tf.shape(state.cell_state.c),
-                                     # "cell_state_h": tf.shape(state.cell_state.h),
-                                     # "attention": tf.shape(state.attention),
-                                     # "time": state.time,
-                                     # "alignments": tf.shape(state.alignments),
-                                     # "attention_state": tf.shape(state.attention_state),
-                                     # })
+    add_dict_to_collection("debug", {"final_time": final_time,
+                                     "sequence_mask_sub_shape": tf.shape(sequence_mask_sub),
+                                     "outputs.rnn_output shape": tf.shape(outputs.rnn_output),
+                                     "outputs_sub.rnn_output shape": tf.shape(outputs_sub.rnn_output),
+                                     "cell_state_c": tf.shape(state.cell_state.c),
+                                     "cell_state_h": tf.shape(state.cell_state.h),
+                                     "attention": tf.shape(state.attention),
+                                     "time": state.time,
+                                     "alignments": tf.shape(state.alignments),
+                                     "attention_state": tf.shape(state.attention_state),
+                                     })
 
     if fused_projection and output_layer_master is not None:
         logits = output_layer_master(outputs.rnn_output)

opennmt/models/hierarchical_seq2seq.py
@@ -90,10 +90,9 @@ class HierarchicalSequenceToSequence(Model):
 
       tf.logging.info(log_separator+" >> [hierarchical_seq2seq.py _build] mode = {}".format(mode))
       tf.logging.info(" >> [hierarchical_seq2seq.py _build] features = \n{}".format(features))
+      tf.logging.info(" >> [hierarchical_seq2seq.py _build] len(features) = {}\n".format(len(features)))
       tf.logging.info(" >> [hierarchical_seq2seq.py _build] labels = \n{}".format(labels))
-
-      if mode == tf.estimator.ModeKeys.TRAIN:
-          labels = (labels[0][0], labels[1][0])
+      tf.logging.info(" >> [hierarchical_seq2seq.py _build] len(labels) = {}".format(len(labels) if labels is not None else None))
 
       features_length = self._get_features_length(features)
       log_dir = config.model_dir if config is not None else None
@@ -132,7 +131,6 @@ class HierarchicalSequenceToSequence(Model):
 
       tf.logging.info(" >> [hierarchical_seq2seq.py _build] target_embedding_fn = {}".format(target_embedding_fn))
       tf.logging.info(" >> [hierarchical_seq2seq.py _build] target_inputter = {}".format(self.target_inputter))
-      tf.logging.info(" >> [hierarchical_seq2seq.py _build] labels = {}".format(labels))
       if labels is not None:
           master_labels, sub_labels = labels
           target_inputs = _maybe_reuse_embedding_fn(
@@ -171,6 +169,11 @@ class HierarchicalSequenceToSequence(Model):
           tf.logging.info(" >> [hierarchical_seq2seq.py _build] state = {}".format(state))
           tf.logging.info(" >> [hierarchical_seq2seq.py _build] length = {}".format(length))
           tf.logging.info(" >> [hierarchical_seq2seq.py _build] sequence_mask_sub = {}".format(sequence_mask_sub))
+          add_dict_to_collection("debug", {"decoder_logits": tf.shape(logits),
+                                           "decoder_logits_sub": tf.shape(logits_sub),
+                                           "decoder_length": tf.shape(length),
+                                           "sequence_mask_sub": tf.shape(sequence_mask_sub)
+                                           })
       else:
           logits = None
           logits_sub = None
@@ -201,6 +204,13 @@ class HierarchicalSequenceToSequence(Model):
                       memory_sequence_length=encoder_sequence_length,
                       dtype=target_dtype,
                       return_alignment_history=True)
+                  tf.logging.info(" >> [hierarchical_seq2seq.py _build] sampled_ids = {}".format(sampled_ids))
+                  tf.logging.info(" >> [hierarchical_seq2seq.py _build] sampled_length = {}".format(sampled_length))
+                  tf.logging.info(" >> [hierarchical_seq2seq.py _build] log_probs = {}".format(log_probs))
+                  tf.logging.info(" >> [hierarchical_seq2seq.py _build] alignment = {}".format(alignment))
+                  add_dict_to_collection("debug", {"log_probs": tf.shape(log_probs),
+                                                   "alignment": tf.shape(alignment)
+                                                   })
               else:
                   tf.logging.info(" >> [hierarchical_seq2seq.py _build] dynamic_decode ...")
                   length_penalty = params.get("length_penalty", 0)
@@ -231,6 +241,12 @@ class HierarchicalSequenceToSequence(Model):
             assert isinstance(sampled_length, tuple)
             sampled_length, sub_sampled_length = sampled_length
             master_ids, sub_ids = sampled_ids
+            add_dict_to_collection("debug", {"sampled_length": tf.shape(sampled_length),
+                                             "sub_sampled_length": tf.shape(sub_sampled_length),
+                                             "master_ids": tf.shape(master_ids),
+                                             "sub_ids": tf.shape(sub_ids)
+                                             })
+
             target_tokens = target_vocab_rev.lookup(tf.cast(master_ids, tf.int64))
             target_tokens_sub = target_vocab_rev.lookup(tf.cast(sub_ids, tf.int64))
           else:
@@ -339,8 +355,7 @@ class HierarchicalSequenceToSequence(Model):
     """
     tf.logging.info(" >> [hierarchical_seq2seq.py _get_labels_length] labels = {}".format(labels))
     tf.logging.info(" >> [hierarchical_seq2seq.py _get_labels_length] len(labels) = {}".format(len(labels)))
-    master_labels = labels[0]
-    sub_labels = labels[1]
+    master_labels, sub_labels = labels
     tf.logging.info(" >> [hierarchical_seq2seq.py _get_labels_length] master_labels = {}".format(master_labels))
     tf.logging.info(" >> [hierarchical_seq2seq.py _get_labels_length] sub_labels = {}".format(sub_labels))
 
@@ -382,23 +397,28 @@ class HierarchicalSequenceToSequence(Model):
   def _compute_loss(self, features, labels, outputs, params, mode):
 
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] outputs = {}".format(outputs))
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        labels = (labels[0][0], labels[1][0])
     master_labels, sub_labels = labels
     master_logits, sub_logits, sequence_mask_sub = outputs
     master_length, sub_length = self._get_labels_length(labels, to_reduce=True)
+    tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] master_length = {}".format(master_length))
+    tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] sub_length = {}".format(sub_length))
 
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] \nmaster_labels : \n{}".format("\n".join(["{}".format(x) for x in master_labels.items()])))
-
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] BEFORE \nsub_labels : \n{}".format("\n".join(["{}".format(x) for x in sub_labels.items()])))
     sub_labels = self.sub_target_inputter._transform_sub_labels(sub_labels)
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] AFTER \nsub_labels : \n{}".format("\n".join(["{}".format(x) for x in sub_labels.items()])))
 
-    tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] sub_length = {}".format(sub_length))
+    add_dict_to_collection("debug", {"master_length": tf.shape(master_length),
+                                     "sub_length": tf.shape(sub_length),
+                                     "master_logits": tf.shape(master_logits),
+                                     "sub_logits": tf.shape(sub_logits),
+                                     "sequence_mask_sub_shape": tf.shape(sequence_mask_sub),
+                                     "sequence_mask_sub": sequence_mask_sub
+                                     })
+
     sub_loss = self._compute_loss_impl(sub_logits, sub_labels, sub_length, params, mode)
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] sub_loss = {}".format(sub_loss))
 
-    tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] master_length = {}".format(master_length))
     master_loss = self._compute_loss_impl(master_logits, master_labels, master_length, params, mode)
     tf.logging.info(" >> [hierarchical_seq2seq.py _compute_loss] master_loss = {}".format(master_loss))
 

opennmt/models/model.py
@@ -71,59 +71,130 @@ class Model(object):
       """Single callable to compute the loss."""
       tf.logging.info(" >> [model.py model_fn _loss_op] <TRAIN> Building Graph ... ")
       logits, _ = self._build(features, labels, params, mode, config=config) # logits, predictions
+      tf.logging.info(" >> [model.py model_fn _loss_op] <TRAIN> Computing loss ... ... logits = {}".format(logits))
       tf.logging.info(" >> [model.py model_fn _loss_op] <TRAIN> Computing loss ... ... ")
       return self._compute_loss(features, labels, logits, params, mode)
 
-    def _normalize_loss(num, den=None):
+    def _normalize_loss_list_or_single(num, den=None):
       """Normalizes the loss."""
-      if isinstance(num, list):  # Sharded mode.
+      # TODO: come back here, it may be tf.reduce_sum([n/d for n,d in zip(num,den)])
+      # sharded mode
+      if isinstance(num, list):
         if den is not None:
           assert isinstance(den, list)
           return tf.add_n(num) / tf.add_n(den)
         else:
           return tf.reduce_mean(num)
+      # non-sharded mode
       elif den is not None:
         return num / den
       else:
         return num
 
     def _extract_loss(loss):
-      """Extracts and summarizes the loss."""
-      tf.logging.info(" >> [model.py model_fn _extract_loss] BEFORE loss = {}".format(loss))
-      if isinstance(loss, list):
-        loss = loss[0]
-      tf.logging.info(" >> [model.py model_fn _extract_loss] AFTER loss = {}".format(loss))
-
-      def _normalize_loss_meta(loss):
-        tf.logging.info(" >> [model.py model_fn _extract_loss] _normalize_loss_meta : loss = {}".format(loss))
-        if not isinstance(loss, tuple):
-          actual_loss = _normalize_loss(loss)
-          tboard_loss = actual_loss
-        else:
-          actual_loss = _normalize_loss(loss[0], den=loss[1])
-          tboard_loss = _normalize_loss(loss[0], den=loss[2]) if len(loss) > 2 else actual_loss
+      """
+      Extracts and summarizes the loss.
+      NOTE: loss is guaranteed to be a tuple by dispatcher if the _compute_loss() returns more than one outputs
+      """
+      def _normalize_loss_tuple(loss):
+        actual_loss = _normalize_loss_list_or_single(loss[0], den=loss[1])
+        tboard_loss = _normalize_loss_list_or_single(loss[0], den=loss[2]) if len(loss) > 2 else actual_loss
         return actual_loss, tboard_loss
 
-      # single loss to be normalized
-      if not isinstance(loss, tuple):
-        actual_loss = _normalize_loss_meta(loss)
-        tboard_loss = actual_loss
-      # multiple losses to be normalized
-      else:
-        master_loss, sub_loss = loss
-        tf.logging.info(" >> [model.py model_fn _extract_loss] master_loss = {}".format(master_loss))
-        tf.logging.info(" >> [model.py model_fn _extract_loss] sub_loss = {}".format(sub_loss))
+      def listoftuple_to_tupleoflist(input):
+        return tuple(list(i) for i in zip(*input))
 
-        master_actual_loss, master_tboard_loss = _normalize_loss_meta(master_loss)
-        sub_actual_loss, sub_tboard_loss = _normalize_loss_meta(sub_loss)
+      def _normalize_master_and_sub_losses(loss):
+        """
+          sharded, multiple losses
+        """
+        master_loss_list, sub_loss_list = loss
+        tf.logging.info(" >> [model.py model_fn _extract_loss] master_loss = {}".format(master_loss_list))
+        tf.logging.info(" >> [model.py model_fn _extract_loss] sub_loss = {}".format(sub_loss_list))
+
+        master_loss_tuple_of_shards = listoftuple_to_tupleoflist(master_loss_list)
+        sub_loss_tuple_of_shards = listoftuple_to_tupleoflist(sub_loss_list)
+
+        tf.logging.info(" >> [model.py model_fn _extract_loss] master_loss_tuple_of_shards = {}".format(master_loss_tuple_of_shards))
+        tf.logging.info(" >> [model.py model_fn _extract_loss] sub_loss_tuple_of_shards = {}".format(sub_loss_tuple_of_shards))
+
+        master_actual_loss, master_tboard_loss = _normalize_loss_tuple(master_loss_tuple_of_shards)
+        sub_actual_loss, sub_tboard_loss = _normalize_loss_tuple(sub_loss_tuple_of_shards)
+
         tf.logging.info(" >> [model.py model_fn _extract_loss] master_actual_loss = {}".format(master_actual_loss))
         tf.logging.info(" >> [model.py model_fn _extract_loss] master_tboard_loss = {}".format(master_tboard_loss))
         tf.logging.info(" >> [model.py model_fn _extract_loss] sub_actual_loss = {}".format(sub_actual_loss))
         tf.logging.info(" >> [model.py model_fn _extract_loss] sub_tboard_loss = {}".format(sub_tboard_loss))
+
+        add_dict_to_collection("debug", {"master_actual_loss": master_actual_loss,
+                                         "master_tboard_loss": master_tboard_loss,
+                                         "sub_actual_loss": sub_actual_loss,
+                                         "sub_tboard_loss": sub_tboard_loss,
+                                         })
+
         actual_loss = tf.reduce_mean([master_actual_loss, sub_actual_loss])
         tboard_loss = tf.reduce_mean([master_tboard_loss, sub_tboard_loss])
 
-      tf.summary.scalar("loss normalized by sequence length)", tboard_loss)
+        tf.summary.scalar("master_actual_loss_loss_normalized_by_length)", master_actual_loss)
+        tf.summary.scalar("master_tboard_loss_normalized_by_length)", master_tboard_loss)
+        tf.summary.scalar("sub_actual_loss_normalized_by_length)", sub_actual_loss)
+        tf.summary.scalar("sub_tboard_loss_normalized_by_length)", sub_tboard_loss)
+
+        return actual_loss, tboard_loss
+
+      tf.logging.info(" >> [model.py model_fn _extract_loss] un-normalized loss = {}".format(loss))
+
+      if not isinstance(loss, tuple):
+        '''
+          loss is a single value, or list of values from shards
+        '''
+        tf.logging.info(" >> [model.py model_fn _extract_loss] loss is not tuple")
+        actual_loss = _normalize_loss_list_or_single(loss)
+        tboard_loss = actual_loss
+
+      else:
+        '''
+          loss is a tuple (_compute_loss() returns more than one output)
+          elements can be lists
+        '''
+        if not isinstance(loss[0], list):
+          '''
+            non-sharded: each element of this tuple is exactly what _compute_loss() returns in the same order
+          '''
+          if not isinstance(loss[0], tuple):
+            '''
+              (loss, loss_normalizer_1, loss_normalizer_2)
+            '''
+            actual_loss, tboard_loss = _normalize_loss_tuple(loss)
+          else:
+            '''
+              (master_loss_tuple, sub_loss_tuple)
+            '''
+            master_loss_tuple, sub_loss_tuple = loss
+            master_actual_loss, master_tboard_loss = _normalize_loss_tuple(master_loss_tuple)
+            sub_actual_loss, sub_tboard_loss = _normalize_loss_tuple(sub_loss_tuple)
+            actual_loss = tf.reduce_mean([master_actual_loss, sub_actual_loss])
+            tboard_loss = tf.reduce_mean([master_tboard_loss, sub_tboard_loss])
+
+        else:
+          '''
+            sharded: each element of this tuple is list of shards (same as what _compute_loss() returns in order)
+          '''
+          # single loss to be normalized
+          if not isinstance(loss[0][0], tuple):
+            '''
+              look at the 0th list, the 0th element, if not a tuple then it is the loss in (loss, loss_normalizer_1, loss_normalizer_2)
+            '''
+            actual_loss, tboard_loss = _normalize_loss_tuple(loss)
+          # multiple losses to be normalized, in this case master_loss and sub_loss
+          else:
+            '''
+              else it is the master_loss_tuple in (master_loss_tuple, sub_loss_tuple)
+            '''
+            actual_loss, tboard_loss = _normalize_master_and_sub_losses(loss)
+
+      tf.summary.scalar("actual_loss", actual_loss)
+      tf.summary.scalar("tboard_loss", tboard_loss)
       return actual_loss
 
     def _model_fn(features, labels, params, mode, config):
@@ -138,7 +209,7 @@ class Model(object):
                         such as num_ps_replicas, or model_dir
       :return: ops necessary to perform training, evaluation, or predictions.
       """
-      tf.logging.info(log_separator+" >> [model.py model_fn _model_fn] \n---\nfeatures = {}\n---\nlabels = {}".format(features, labels))
+      tf.logging.info(log_separator+" >> [model.py model_fn _model_fn] \nfeatures = {}\n---\nlabels = {}".format(features, labels))
 
       # *********************************************************************** #
       # ******************************** Train ******************************** #
@@ -150,16 +221,25 @@ class Model(object):
         tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> dispatching shards ... ")
         features_shards = dispatcher.shard(features)
         tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> features_shards = \n{} ".format(features_shards))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> len(features_shards) = {} ".format(len(features_shards)))
+
         if isinstance(labels, tuple):
-          labels_shards = tuple([dispatcher.shard(l) for l in labels])
+          sharded = (dispatcher.shard(l) for l in labels)
+          tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> sharded = {} \n\n".format(sharded))
+          labels_shards = [x for x in zip(*sharded)]
         else:
           labels_shards = dispatcher.shard(labels)
-        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> labels_shards = \n{} ".format(labels_shards))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> labels_shards = {} ".format(labels_shards))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> len(labels_shards) = {} ".format(len(labels_shards)))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> type(labels_shards[0]) = {} ".format(type(labels_shards[0])))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> len(labels_shards[0]) = {} ".format(len(labels_shards[0])))
 
         tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> Creating loss_ops ...")
         with tf.variable_scope(self.name, initializer=self._initializer(params), reuse=tf.AUTO_REUSE):
           losses_shards = dispatcher(_loss_op, features_shards, labels_shards, params, mode, config)
 
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> losses_shards = \n{}".format(losses_shards))
+        tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> len(losses_shards) = {}\n".format(len(losses_shards)))
         tf.logging.info(" >> [model.py model_fn _model_fn] <TRAIN> Extracts and summarizes the loss ...")
         loss = _extract_loss(losses_shards)
 
@@ -453,7 +533,6 @@ class Model(object):
       process_fn = lambda features, labels: (feat_process_fn(features), labels_process_fn(labels))
 
       tf.logging.info(" >> [model.py _input_fn_impl] maximum_labels_length = {} ".format(maximum_labels_length))
-      add_dict_to_collection("debug", {"maximum_labels_length": maximum_labels_length})
 
     if mode == tf.estimator.ModeKeys.TRAIN:
       tf.logging.info(log_separator+" >> [model.py _input_fn_impl] Building training_pipeline ... ")
@@ -475,7 +554,6 @@ class Model(object):
         labels_length_fn=self._get_labels_length)
     else:
       tf.logging.info(log_separator+" >> [model.py _input_fn_impl] Building inference_pipeline ... ")
-      # TODO: check this
       dataset = data.inference_pipeline(
           dataset,
           batch_size,

opennmt/utils/hooks.py
@@ -58,13 +58,14 @@ class CountersHook(tf.train.SessionRunHook):
                every_n_steps=100,
                every_n_secs=None,
                output_dir=None,
-               summary_writer=None):
+               summary_writer=None,
+               debug=False):
     if (every_n_steps is None) == (every_n_secs is None):
       raise ValueError("exactly one of every_n_steps and every_n_secs should be provided.")
     self._timer = tf.train.SecondOrStepTimer(
         every_steps=every_n_steps,
         every_secs=every_n_secs)
-
+    self._debug = debug
     self._summary_writer = summary_writer
     self._output_dir = output_dir
 
@@ -81,7 +82,8 @@ class CountersHook(tf.train.SessionRunHook):
     if self._global_step is None:
       raise RuntimeError("Global step should be created to use WordCounterHook.")
 
-    # self._debug_ops = misc.get_dict_from_collection("debug")
+    if self._debug:
+      self._debug_ops = misc.get_dict_from_collection("debug")
 
   def before_run(self, run_context):  # pylint: disable=unused-argument
     '''
@@ -106,8 +108,10 @@ class CountersHook(tf.train.SessionRunHook):
     '''
     if not self._counters:
       return None
-    return tf.train.SessionRunArgs([self._counters, self._global_step])
-    # return tf.train.SessionRunArgs([self._counters, self._global_step, self._debug_ops])
+    run_args = [self._counters, self._global_step]
+    if self._debug:
+      run_args.append(self._debug_ops)
+    return tf.train.SessionRunArgs(run_args)
 
   def after_run(self, run_context, run_values):  # pylint: disable=unused-argument
     '''
@@ -119,10 +123,12 @@ class CountersHook(tf.train.SessionRunHook):
     '''
 
     if not self._counters:return
-    counters, step = run_values.results
-    # counters, step, debug_ops = run_values.results
-    # tf.logging.info(" >> [hooks.py class CountersHook after_run] debug_ops : ")
-    # pp.pprint(debug_ops)
+    if self._debug:
+      counters, step, debug_ops = run_values.results
+      tf.logging.info(" >> [hooks.py class CountersHook after_run] debug_ops : ")
+      pp.pprint(debug_ops)
+    else:
+      counters, step = run_values.results
     if self._timer.should_trigger_for_step(step):
       elapsed_time, _ = self._timer.update_last_triggered_step(step)
       if elapsed_time is not None:
@@ -140,22 +146,32 @@ class CountersHook(tf.train.SessionRunHook):
 class LogPredictionTimeHook(tf.train.SessionRunHook):
   """Hooks that gathers and logs prediction times."""
 
+  def __init__(self, debug=False):
+    self._debug = debug
+
   def begin(self):
     self._total_time = 0
     self._total_tokens = 0
     self._total_examples = 0
-    self._debug_ops = misc.get_dict_from_collection("debug")
+    if self._debug:
+      self._debug_ops = misc.get_dict_from_collection("debug")
 
   def before_run(self, run_context):
     self._run_start_time = time.time()
     predictions = run_context.original_args.fetches
-    return tf.train.SessionRunArgs([predictions, self._debug_ops])
+    run_args = [predictions]
+    if self._debug:
+      run_args.append(self._debug_ops)
+    return tf.train.SessionRunArgs(run_args)
 
   def after_run(self, run_context, run_values):  # pylint: disable=unused-argument
     self._total_time += time.time() - self._run_start_time
-    predictions, debug_ops = run_values.results
-    tf.logging.info(" >> [hooks.py class LogPredictionTimeHook after_run] debug_ops :")
-    pp.pprint(debug_ops)
+    if self._debug:
+      predictions, debug_ops = run_values.results
+      tf.logging.info(" >> [hooks.py class LogPredictionTimeHook after_run] debug_ops :")
+      pp.pprint(debug_ops)
+    else:
+      predictions = run_values.results
     batch_size = next(six.itervalues(predictions)).shape[0]
     self._total_examples += batch_size
     length = predictions.get("length")
@@ -174,7 +190,7 @@ class LogPredictionTimeHook(tf.train.SessionRunHook):
 class SaveEvaluationPredictionHook(tf.train.SessionRunHook):
   """Hook that saves the evaluation predictions."""
 
-  def __init__(self, model, output_file, post_evaluation_fn=None):
+  def __init__(self, model, output_file, post_evaluation_fn=None, debug=False):
     """Initializes this hook.
 
     Args:
@@ -185,6 +201,7 @@ class SaveEvaluationPredictionHook(tf.train.SessionRunHook):
         current step and the file with the saved predictions.
     """
     self._model = model
+    self._debug = debug
     self._output_file = output_file
     self._post_evaluation_fn = post_evaluation_fn
 
@@ -203,28 +220,33 @@ class SaveEvaluationPredictionHook(tf.train.SessionRunHook):
     self._global_step = tf.train.get_global_step()
     if self._global_step is None:
       raise RuntimeError("Global step should be created to use SaveEvaluationPredictionHook.")
-    # self._debug_ops = misc.get_dict_from_collection("debug")
+    if self._debug:
+      self._debug_ops = misc.get_dict_from_collection("debug")
 
   def before_run(self, run_context):  # pylint: disable=unused-argument
-    # return tf.train.SessionRunArgs([self._predictions, self._global_step])
-    return tf.train.SessionRunArgs([self._predictions, self._global_step])
-    # return tf.train.SessionRunArgs([self._predictions, self._global_step, self._debug_ops])
+    run_args = [self._predictions, self._global_step]
+    if self._debug:
+      run_args.append(self._debug_ops)
+    return tf.train.SessionRunArgs(run_args)
 
   def after_run(self, run_context, run_values):  # pylint: disable=unused-argument
-    predictions, self._current_step = run_values.results
-    # tf.logging.info(" >> [hooks.py class SaveEvaluationPredictionHook after_run] debug_ops")
-    # pp.pprint(debug_ops)
-    # predictions, self._current_step = run_values.results
+    if self._debug:
+      predictions, self._current_step, debug_ops = run_values.results
+      tf.logging.info(" >> [hooks.py class SaveEvaluationPredictionHook after_run] debug_ops")
+      pp.pprint(debug_ops)
+    else:
+      predictions, self._current_step = run_values.results
     self._output_path = "{}.{}".format(self._output_file, self._current_step)
     with io.open(self._output_path, encoding="utf-8", mode="a") as output_file:
       if "tokens_sub" in six.iterkeys(predictions):
         self._output_path_sub = "{}.sub.{}".format(self._output_file, self._current_step)
-        output_file_sub = io.open(self._output_path_sub, encoding="utf-8", mode="a")
+        with io.open(self._output_path_sub, encoding="utf-8", mode="a") as output_file_sub:
+          for prediction in misc.extract_batches(predictions):
+            self._model.print_prediction(prediction, stream=output_file, sub_stream=output_file_sub)
       else:
-        output_file_sub = None
-      for prediction in misc.extract_batches(predictions):
-        self._model.print_prediction(prediction, stream=output_file, sub_stream=output_file_sub)
-      output_file_sub.close()
+        for prediction in misc.extract_batches(predictions):
+          self._model.print_prediction(prediction, stream=output_file)
+
 
   def end(self, session):
     tf.logging.info("Running _post_evaluation_fn (BLEU); Evaluation predictions saved to %s", self._output_path)