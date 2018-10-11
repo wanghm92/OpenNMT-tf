ba9fa678d54d550557e6b1be6e901ed4fcf325c7
wanghm92 <hongmin_wang@ucsb.edu>
2018-10-11 00:41:49 -0700

    added some models/configs for attribute-fragment 1to1 training

---
+++ config/models/recom_nlg_attr_fragment_1to1_lstm.py
@@ -0,0 +1,37 @@
+"""Defines a sequence to sequence model with multiple input features. For
+example, this could be words, parts of speech, and lemmas that are embedded in
+parallel and concatenated into a single input embedding. The features are
+separate data files with separate vocabularies.
+"""
+
+import tensorflow as tf
+import opennmt as onmt
+from opennmt.models.sequence_to_sequence import EmbeddingsSharingLevel
+
+def model():
+  return onmt.models.SequenceToSequence(
+      source_inputter=onmt.inputters.ParallelInputter([
+          onmt.inputters.WordEmbedder(
+              vocabulary_file_key="source_words_vocabulary",
+              embedding_size=None,
+              embedding_file_key="words_embedding"),
+          onmt.inputters.WordEmbedder(
+              vocabulary_file_key="feature_vocabulary",
+              embedding_size=16)],
+          reducer=onmt.layers.ConcatReducer()),
+      target_inputter=onmt.inputters.WordEmbedder(
+          vocabulary_file_key="target_words_vocabulary",
+          embedding_size=None,
+          embedding_file_key="words_embedding"),
+      encoder=onmt.encoders.ConvEncoder(
+          num_layers=1,
+          kernel_size=1,
+          num_units=256),
+      decoder=onmt.decoders.RNNDecoder(
+          num_layers=1,
+          num_units=128,
+          bridge=onmt.layers.DenseBridge(),
+          cell_class=tf.contrib.rnn.LSTMCell,
+          dropout=0.3,
+          residual_connections=False),
+      share_embeddings=EmbeddingsSharingLevel.NONE)
